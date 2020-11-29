a6afe9af03c4d8df870d88297d65f78f9bc1776d
babich kyrylo <59411913+Kirill-bab@users.noreply.github.com>
2020-11-29 20:12:50 +0200

    Update RearrangeDigits.cs

--- RearrangeDigits.cs
+++ RearrangeDigits.cs
@@ -10,30 +10,55 @@ namespace HomeWork1
     {
         public static int Rearrange(int input)
         {
+            int k = 0, smallest;
             string inputToStr = input.ToString();
             int[] digitsArray = new int[inputToStr.Length];
 
             for (int i = 0; i < digitsArray.Length; i++)
             {
-                digitsArray[i] = int.Parse(inputToStr[i].ToString());                // перевожу число в массив цифр
+                digitsArray[i] = int.Parse(inputToStr[i].ToString());                
             }
 
             int[] newDigitsArray = new int[digitsArray.Length];
             Array.Copy(digitsArray, newDigitsArray, digitsArray.Length);
-            
-            for (int i = newDigitsArray.Length - 1; i > 0; i--)
+
+            for (k = newDigitsArray.Length - 1; k > 0; k--)
             {
-                if (newDigitsArray[i] > newDigitsArray[i - 1])
+                if(newDigitsArray[k] > newDigitsArray[k - 1])
                 {
-                    int temp = newDigitsArray[i];
-                    newDigitsArray[i] = newDigitsArray[i - 1];
-                    newDigitsArray[i - 1] = temp;
                     break;
                 }
-            }  
-
-            if (string.Join("", newDigitsArray) == string.Join("", digitsArray))
+            } 
+            
+            if (k == 0)
                 return -1;
+
+            smallest = k;
+            int minMax = k-1;
+            for (int i = k+1; i < newDigitsArray.Length; i++)
+            {
+                if(newDigitsArray[i] > newDigitsArray[minMax] && newDigitsArray[i] < newDigitsArray[smallest] )
+                {
+                    smallest = i;
+                }
+            }
+
+            int temp = newDigitsArray[smallest];
+            newDigitsArray[smallest] = newDigitsArray[minMax];
+            newDigitsArray[minMax] = temp;
+
+            for (int i = k; i < newDigitsArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j < newDigitsArray.Length; j++)
+                {
+                    if (newDigitsArray[i] > newDigitsArray[j])
+                    {
+                        temp = newDigitsArray[i];
+                        newDigitsArray[i] = newDigitsArray[j];
+                        newDigitsArray[j] = temp;
+                    }
+                }
+            }
 
             int result = int.Parse(String.Join("", newDigitsArray));
 
